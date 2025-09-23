573a48622146c0d79ada35d6787bcadaf25ead70
kimeunkyoungg <127072393+kimeunkyoungg@users.noreply.github.com>
2025-09-24 08:59:15 +0900

    Update README.md

--- backend/src/main/java/com/backend/domain/product/product/entity/Product.java
+++ backend/src/main/java/com/backend/domain/product/product/entity/Product.java
@@ -1,4 +1,30 @@
 package com.backend.domain.product.product.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;  // 상품 이름
+
+    @Column(columnDefinition = "TEXT")
+    private String description;  // 상세 설명
+
+    @Column(nullable = false)
+    private int price;  // 가격
+
+    @Column(name = "image_url")
+    private String imageUrl;  // 상품 이미지 경로
+
 }
