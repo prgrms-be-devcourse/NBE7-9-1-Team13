e7e1077f84940763bc33ed435f20f9f28fe15ffa
kimeunkyoungg <127072393+kimeunkyoungg@users.noreply.github.com>
2025-09-29 16:36:32 +0900

    feat:token 만료시간 변경#61

--- backend/src/main/java/com/backend/domain/item/item/controller/AdminItemController.java
+++ backend/src/main/java/com/backend/domain/item/item/controller/AdminItemController.java
@@ -1,25 +1,16 @@
 package com.backend.domain.item.item.controller;
 
-import java.util.List;
-
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.backend.domain.item.item.dto.ItemCreateRequest;
 import com.backend.domain.item.item.dto.ItemResponse;
 import com.backend.domain.item.item.dto.ItemUpdateRequest;
 import com.backend.domain.item.item.service.ItemService;
 import com.backend.global.rsData.RsData;
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/admin/items")
