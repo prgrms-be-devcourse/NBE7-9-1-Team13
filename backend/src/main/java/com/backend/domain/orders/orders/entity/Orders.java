b66d7f8e7f49fb06538b5d9deae5e3c1cad8920f
choijihyuk <jihyuk0609@naver.com>
2025-09-29 14:17:16 +0900

    develop에서 받아ë온 내용 임시 저장

--- backend/src/main/java/com/backend/domain/orders/orders/entity/Orders.java
+++ backend/src/main/java/com/backend/domain/orders/orders/entity/Orders.java
@@ -1,15 +1,25 @@
 package com.backend.domain.orders.orders.entity;
 
-import com.backend.domain.orderitem.orderitem.entity.OrderItem;
-import com.backend.global.jpa.entity.BaseEntity;
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+
+import com.backend.domain.member.member.entity.Member;
+import com.backend.domain.orderitem.orderitem.entity.OrderItem;
+import com.backend.global.jpa.entity.BaseEntity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
@@ -30,6 +40,10 @@ public class Orders extends BaseEntity {
     private Status status = Status.ORDERED;
 
     private LocalDateTime deliveryDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = true)
+    private Member member;
 
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<OrderItem> orderItems = new ArrayList<>();
